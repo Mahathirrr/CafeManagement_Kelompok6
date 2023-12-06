be974f05d572052dd9835213fd2332746ecc323c
T. Farhan Syah <t_farhan0@mhs.usk.ac.id>
2023-12-06 17:36:08 +0700

    farhan

--- src/cafeguii/app/CoffeeShopCalculator.java
+++ src/cafeguii/app/CoffeeShopCalculator.java
@@ -1,5 +1,18 @@
 package cafeguii.app;
 
 public class CoffeeShopCalculator {
+    private double total;
+    private int x;
+    private double tax;
 
+    public CoffeeShopCalculator() {
+        total = 0.0;
+        x = 0;
+        tax = 0.0;
+    }
+
+    // Implement your tax calculation logic here
+    public double calculateTax(double subtotal) {
+        return subtotal * 0.11;
+    }
 }
