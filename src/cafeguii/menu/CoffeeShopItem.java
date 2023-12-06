76c7fae9399b66abec9cf912d190e3eedc3a5af7
Pryta Rosela <prytarosela03@gmail.com>
2023-12-06 17:03:08 +0700

    init feature/pryta

--- src/cafeguii/menu/CoffeeShopItem.java
+++ src/cafeguii/menu/CoffeeShopItem.java
@@ -3,5 +3,41 @@ package cafeguii.menu;
 import javax.swing.*;
 
 public class CoffeeShopItem implements MenuItem {
-    
+    private String name;
+    private double price;
+    private ImageIcon icon;
+    private JLabel label; // Add this line
+
+    public CoffeeShopItem(String name, double price, String imagePath, JLabel label) {
+        this.name = name;
+        this.price = price;
+        this.icon = createImageIcon(imagePath);
+        this.label = label;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public ImageIcon getIcon() {
+        return icon;
+    }
+
+    public JLabel getLabel() {
+        return label;
+    }
+
+    private ImageIcon createImageIcon(String path) {
+        java.net.URL imgURL = getClass().getResource(path);
+        if (imgURL != null) {
+            return new ImageIcon(imgURL);
+        } else {
+            System.err.println("Couldn't find file: " + path);
+            return null;
+        }
+    }
 }
